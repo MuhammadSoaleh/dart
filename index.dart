903bc581aea2e2486931a8702f0baa5b5e43c90e
MuhammadSoaleh <m.soaleh63@gmail.com>
2024-05-14 15:59:26 +0500

    first commit

---
+++ index.dart
@@ -0,0 +1,50 @@
+
+import 'dart:io';
+
+void main(){
+  // print("hello world");
+  // print("enter your number");
+  // int? one  = int.parse(stdin.readLineSync()!);
+  // print("enter your number two");
+  // int? two  = int.parse(stdin.readLineSync()!);
+  // print("the sum is ${one + two}");
+  
+//   print("please! enter your age");
+//   int? age= int.parse(stdin.readLineSync()!);
+//   if (age <= 10) {
+//     print("you are a kid!");
+//   } else if (age <= 19){
+//     print("you are a teenager!");
+//   }
+//   else if (age <=29 ){
+// print("you are a young");
+//   }
+//   else if (age <=39 ){
+// print("you are a adult");
+//   }
+//   else if (age >=40 ){
+// print("you are a senior citizen");
+//   }
+// print("Enter your bill units");
+// int? units=int.parse(stdin.readLineSync()!);
+// if (units <=10) {
+//   print("your units is 15/$units");
+//   print("total amount is {int 15*$units}");
+//   int a = 15*units;
+//   print("$a");
+// }
+// int res=1;
+// for (var i = 0; i <= 10; i++) {
+//   res+=i;
+// }
+// print(res);
+// print("enter a number");
+// int a= int.parse(stdin.readLineSync()!);
+// int res=1;
+// for (var i = 1; i <=a; i++) {
+//   res=res*i;
+//   // print(i);
+// }
+// print(res);
+}
+// This is a comment. Comments are ignored by the JavaScript interpreter, so they don'
